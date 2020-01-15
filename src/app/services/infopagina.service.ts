3636decf402e8a271260001d80b83a36ec602d83
Omar Bonilla <obonilla@solati.com.co>
2020-01-15 17:52:47 -0500

    Fin sección 5

--- src/app/services/infopagina.service.ts
+++ src/app/services/infopagina.service.ts
@@ -11,16 +11,36 @@ export class InfopaginaService {
 
 	info: InfoPagina = {};
 	cargada = false;
+  equipo:any[] = [];
 
 
   constructor(private http:HttpClient) {
-   this.http.get('assets/data/data-pagina.json')
-   .subscribe( (resp:InfoPagina) => {
-	   this.cargada=true;
-	   this.info=resp;
 
-   });
-
+  this.cargarinfo();
+  this.cargarequipo();
 
    }
+
+
+   private  cargarinfo(){
+
+       this.http.get('assets/data/data-pagina.json')
+       .subscribe( (resp:InfoPagina) => {
+         this.cargada=true;
+         this.info=resp;
+
+       });
+   }
+
+   private  cargarequipo(){
+
+       this.http.get('https://proyecto-e8ec7.firebaseio.com/equipo.json')
+       .subscribe(resp => {
+       
+         this.equipo=resp;
+
+       });
+   }
+
+
 }
